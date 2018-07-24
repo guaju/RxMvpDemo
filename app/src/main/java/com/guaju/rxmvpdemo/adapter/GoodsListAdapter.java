cc7ea793555cbcbb3200e77805b80e74dc343ee6
guaju <guaju@126.com>
2018-07-24 10:14:10 +0800

    完成

---
+++ app/src/main/java/com/guaju/rxmvpdemo/adapter/GoodsListAdapter.java
@@ -0,0 +1,10 @@
+package com.guaju.rxmvpdemo.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by guaju on 2018/7/24.
+ */
+
+public class GoodsListAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/com/guaju/rxmvpdemo/view/activity/goodslist/GoodsListPresenter.java
@@ -0,0 +1,43 @@
+package com.guaju.rxmvpdemo.view.activity.goods;
+
+import com.guaju.rxmvpdemo.bean.GoodsList;
+import com.guaju.rxmvpdemo.contract.GoodsListContract;
+import com.guaju.rxmvpdemo.engine.GoodsEngine;
+
+/**
+ * Created by guaju on 2018/7/23.
+ */
+
+public class GoodsListPresenter extends GoodsListContract.Presenter {
+    GoodsListContract.View v;
+
+    private GoodsEngine goodsEngine;
+
+    public GoodsListPresenter(GoodsListContract.View v) {
+        super(v);
+    }
+
+    @Override
+    public void getGoodsList() {
+        //获取商品列表      需要去操作model了
+        goodsEngine = new GoodsEngine();
+        goodsEngine.GoodsList("goodsapp", "index_nowsell", new GoodsEngine.OnRequestCompleted<GoodsList>() {
+                    @Override
+                    public void onError() {
+                        v.onError();
+                    }
+
+
+                    @Override
+                    public void onCompleted(GoodsList goodsList) {
+                        v.onCompleted(goodsList);
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
